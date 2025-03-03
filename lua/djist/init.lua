a255deb8810da38afc5390be2a5384fdde2ffce5
Jamsjz <gj984233@gmail.com>
2025-03-03 23:38:46 +0545

    new

--- lua/djist/init.lua
+++ lua/djist/init.lua
@@ -1,14 +1,16 @@
-local M = {}
-local nodes = require('djist.nodes')
-local ts_utils = require('nvim-treesitter.ts_utils')
-local bridge = require('djist.pybridge')
-
 M.showViewURLS = function()
   local view, viewBody = nodes.getViewNodes()
+
+  -- Debugging output
+  print("view:", view)
+  print("viewBody:", viewBody)
+
+  if not view or not viewBody then
+    error("Failed to retrieve view or viewBody. Ensure the query matches and the parser is installed.")
+  end
+
   print(ts_utils.get_node_text(view)[1])
   local viewName = ts_utils.get_node_text(view)[1]
   local sRow, _, _, _ = viewBody:range()
   bridge.runPython({ viewName = viewName, rowNo = sRow })
 end
-
-return M
