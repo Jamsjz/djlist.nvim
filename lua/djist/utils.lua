8cdb07e667d1c5ff710508dddcf3f6094c60dbcc
Jamsjz <gj984233@gmail.com>
2025-03-03 23:30:17 +0545

    new

--- lua/djist/utils.lua
+++ lua/djist/utils.lua
@@ -1,6 +1,6 @@
 local M = {}
 
 M.getBufNo = function() return vim.api.nvim_win_get_buf(0) end
-M.getLang = function() return vim.api.nvim_get_option(M.getBufNo(), 'ft') end
+M.getLang = function() return vim.api.nvim_get_option_value(M.getBufNo(), 'ft') end
 
 return M
